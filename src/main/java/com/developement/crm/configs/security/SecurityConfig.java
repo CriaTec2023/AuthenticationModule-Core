afaf44f3b1bf751784a0e0b8776d1e030500f6c6
lupesms97 <lupesms97@gmail.com>
2023-09-15 15:22:58 -0300

    best way to do a cors config

--- src/main/java/com/developement/crm/configs/security/SecurityConfig.java
+++ src/main/java/com/developement/crm/configs/security/SecurityConfig.java
@@ -1,16 +1,15 @@
-package com.developement.crm.config.security;
+package com.developement.crm.configs.security;
 
-import com.developement.crm.exceptionHandlers.SecurityConfigException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -26,14 +25,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( authorize -> authorize
                             .requestMatchers(HttpMethod.POST,"auth/login").permitAll()
-                                .requestMatchers(HttpMethod.POST,"auth/register").permitAll()
-//                            .requestMatchers(HttpMethod.POST,"auth/register").hasRole("USER")
+                            .requestMatchers(HttpMethod.POST,"auth/register").permitAll()
                             .anyRequest().authenticated()
                 )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(crsf -> crsf.disable())
+                .cors(Customizer.withDefaults())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
 
--- src/main/java/com/developement/crm/configs/security/SecurityFilter.java
+++ src/main/java/com/developement/crm/configs/security/SecurityFilter.java
@@ -1,12 +1,12 @@
-package com.developement.crm.config.security;
+package com.developement.crm.configs.security;
 
-import com.developement.crm.service.AuthenticationService;
-import com.developement.crm.service.TokenService;
+import com.developement.crm.repositories.UsersRepository;
+import com.developement.crm.services.AuthenticationService;
+import com.developement.crm.services.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.apache.catalina.UserDatabase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,27 +22,27 @@ public class SecurityFilter extends OncePerRequestFilter {
     TokenService tokenService;
 
     @Autowired
-    AuthenticationService authenticationService;
+    UsersRepository usersRepository;
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = this.recoversToken(request);
+        var token = this.recoverToken(request);
         if(token != null){
             var login = tokenService.validateToken(token);
 //            .getSubject(): Após a verificação bem-sucedida do token,
 //                você está extraindo o "subject" (assunto) do token.
 //                O assunto é geralmente um identificador único associado ao usuário
 //            ou à entidade que emitiu o token.
-            UserDetails user = authenticationService.loadUserByUsername(login);
+            UserDetails user = usersRepository.findByLogin(login);
+
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
         filterChain.doFilter(request, response);
-
     }
 
-    private String recoversToken(HttpServletRequest request) {
+    private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
         if(authHeader == null || authHeader.isEmpty() || !authHeader.startsWith("Bearer ")){
             return null;
